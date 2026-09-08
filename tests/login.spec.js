e15bd4b19ce35784c85bf5c26eec82e40e2c09d7
nithin-dharavathu <nithindharavath01@gmail.com>
2026-09-09 01:35:54 +0530

    changes made in the login spec

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,11 +4,10 @@ const url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
 
 test("login testing", async ({page}) => {
     await page.goto(url);
-    await console.log(page.title());
+    console.log(await page.title());
     await page.getByPlaceholder("Username").fill("Admin");
     await page.getByPlaceholder("Password").fill("admin123")
     await page.getByRole("button", {name:"Login"}).click();
-    await page.waitForTimeout(6000);
     await expect(page).toHaveURL(/index/i);
 })
 
